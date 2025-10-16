265b1d792d0351ed86f809471431e618518eedcc
John Oliphant <john.thomas.oliphant@gmail.com>
2025-10-15 20:09:50 -0400

    added pi3b server to runner service

--- shepherd/__init__.py
+++ shepherd/__init__.py
@@ -3,7 +3,9 @@ import os
 
 def create_app(test_config=None):
     """The application factory. Creates and configures the Flask app."""
-    app = Flask(__name__, instance_relative_config=True)
+    # By adding template_folder, we explicitly tell Flask to look for the templates
+    # directory one level "up" from this file, which is the project's root directory.
+    app = Flask(__name__, instance_relative_config=True, template_folder='../templates')
     app.secret_key = os.urandom(24)
 
     # Initialize the database
